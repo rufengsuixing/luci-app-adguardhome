5ce60cb0a07785dd2d59268a1dcc8135c8ce0b25
rufengsuixing <1985151814@qq.com>
2019-12-24 12:34:04 +0800

    fix: ban reload fault

--- root/usr/share/AdGuardHome/addhost.sh
+++ root/usr/share/AdGuardHome/addhost.sh
@@ -2,15 +2,14 @@
 checkmd5(){
 local nowmd5=$(md5sum /etc/hosts)
 nowmd5=${nowmd5%% *}
-local lastmd5=$(uci get AdGuardHome.AdGuardHome.hostsmd5)
+local lastmd5=$(uci get AdGuardHome.AdGuardHome.hostsmd5 2>/dev/null)
 if [ "$nowmd5" != "$lastmd5" ]; then
-	/etc/init.d/AdGuardHome reload
 	uci get AdGuardHome.AdGuardHome.hostsmd5="$nowmd5"
-	[ "$2" == "noreload" ] && return
 	uci commit AdGuardHome
+	[ "$1" == "noreload" ] || /etc/init.d/AdGuardHome reload
 fi
 }
-[ "$1" == "del" ] && sed -i '/programaddstart/,/programaddend/d' /etc/hosts && checkmd5 && exit 0
+[ "$1" == "del" ] && sed -i '/programaddstart/,/programaddend/d' /etc/hosts && checkmd5 "$2" && exit 0
 /usr/bin/awk 'BEGIN{
 while ((getline < "/tmp/dhcp.leases") > 0)
 {
@@ -22,7 +21,7 @@ while (("ip -6 neighbor show | grep -v fe80" | getline) > 0)
 }
 print "#programaddend" >"/tmp/tmphost";
 }'
-grep programaddstart /etc/hosts
+grep programaddstart /etc/hosts >/dev/null 2>&1
 if [ "$?" == "0" ]; then
 	sed -i '/programaddstart/,/programaddend/c\#programaddstart' /etc/hosts
 	sed -i '/programaddstart/'r/tmp/tmphost /etc/hosts
@@ -31,4 +30,4 @@ else
 	cat /tmp/tmphost >> /etc/hosts
 fi
 rm /tmp/tmphost
-checkmd5
+checkmd5 "$2"
--- root/usr/share/AdGuardHome/gfw2adg.sh
+++ root/usr/share/AdGuardHome/gfw2adg.sh
@@ -3,17 +3,16 @@ PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 checkmd5(){
 local nowmd5=$(md5sum /tmp/adguard.list 2>/dev/null)
 nowmd5=${nowmd5%% *}
-local lastmd5=$(uci get AdGuardHome.AdGuardHome.gfwlistmd5)
+local lastmd5=$(uci get AdGuardHome.AdGuardHome.gfwlistmd5 2>/dev/null)
 if [ "$nowmd5" != "$lastmd5" ]; then
-	/etc/init.d/AdGuardHome reload
 	uci get AdGuardHome.AdGuardHome.gfwlistmd5="$nowmd5"
-	[ "$2" == "noreload" ] && return
 	uci commit AdGuardHome
+	[ "$1" == "noreload" ] || /etc/init.d/AdGuardHome reload
 fi
 }
-configpath=$(uci get AdGuardHome.AdGuardHome.configpath)
-[ "$1" == "del" ] && sed -i '/programaddstart/,/programaddend/d' $configpath && checkmd5 && exit 0
-gfwupstream=$(uci get AdGuardHome.AdGuardHome.gfwupstream)
+configpath=$(uci get AdGuardHome.AdGuardHome.configpath 2>/dev/null)
+[ "$1" == "del" ] && sed -i '/programaddstart/,/programaddend/d' $configpath && checkmd5 "$2" && exit 0
+gfwupstream=$(uci get AdGuardHome.AdGuardHome.gfwupstream 2>/dev/null)
 if [ -z $gfwupstream ]; then
 gfwupstream="tcp://208.67.220.220:5353"
 fi
@@ -83,5 +82,5 @@ else
 	sed -i '1i\  - '\''[/programaddstart/]#'\''' /tmp/adguard.list
 	sed -i '/upstream_dns:/'r/tmp/adguard.list $configpath
 fi
-checkmd5
+checkmd5 "$2"
 rm -f /tmp/gfwlist.txt /tmp/adguard.list
